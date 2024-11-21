c6314b54982381a4e602b1dba922eccbf05ff49f
nelle-kelly <nellekelly04@gmail.com>
2024-11-21 14:09:25 +0100

    commit

--- hazelcast_cache/utils.py
+++ hazelcast_cache/utils.py
@@ -1,6 +1,7 @@
 from hazelcast import HazelcastClient
 from core.settings import HAZELCAST_CONFIG
 
-def get_hazelcast_client():# instancier a partir de django settings
-   client = HazelcastClient(**HAZELCAST_CONFIG)
+def get_hazelcast_client():
+   config = HAZELCAST_CONFIG
+   client = HazelcastClient(**config)
    return client
